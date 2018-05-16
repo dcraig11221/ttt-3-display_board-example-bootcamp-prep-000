a3538f0faf57d5262f0e80665f7ff059cba563a7
Douglas Craig <dcraig112@gmail.com>
2018-05-16 23:21:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ row = cell + divider + cell + divider + cell
 
 separator = "-----------"
 
-print row + \n + separator + \n + row + \n + separator + \n + row
+print row + '\n' + separator + '\n' + row + '\n' + separator + '\n' + row
 end
